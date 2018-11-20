ee68aaba267025d3e88dd91531327ec9d270525a
Kmayer48 <kenny.mayer3@gmail.com>
2018-11-20 18:22:06 -0500

    audio changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,9 @@ var winCount = 0;
 var lossCount = 0;
 var amountCount = 0;
 
+var audio = document.getElementById("audio");
+audio.volume = 0.4;
+
 //Set amount needed and wins/losses
 var startGame = function() {
 randomNum = Math.floor(Math.random() * (120-19) + 19);
